45cc82b946424d38191bbd6b0cee167c47deea7b
yinquan <quan.yin@ele.me>
2019-04-17 11:27:15 +0800

    加入启动图标,配置URL Schemes

--- GraduationPJ/GraduationPJ/AppDelegate.m
+++ GraduationPJ/GraduationPJ/AppDelegate.m
@@ -22,6 +22,13 @@
 }
 
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"URLSchemes" message:@"打开了" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
+    [alertView show];
+    return YES;
+}
+
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
